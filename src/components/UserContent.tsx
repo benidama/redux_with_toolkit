407be786b20ee44e00624b108b84f0612abaa4ca
Benimana Jean Damascene <benidama02@gmail.com>
2025-09-17 18:48:50 +0300

    adding user components

---
+++ src/components/UserContent.tsx
@@ -0,0 +1,13 @@
+const UserContent = () => {
+  return (
+    <div>
+        <div>
+            <h2>User Information</h2>
+            <p>Name: John Doe</p>
+            <p>Email: john.doe@example.com</p>
+        </div>
+    </div>
+  )
+}
+
+export default UserContent
