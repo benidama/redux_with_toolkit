842526c3c97d25d0c93ae6e2249ea18114ac8269
Benimana Jean Damascene <benidama02@gmail.com>
2025-09-25 23:28:10 +0300

    adding one

---
+++ src/components/MoreImpact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MoreImpact() {
+  return (
+    <div>
+        <h1 className='text-2xl font-bold'>More Impact and Engagement</h1>
+    </div>
+  )
+}
+
+export default MoreImpact
