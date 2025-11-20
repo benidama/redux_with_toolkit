062f22ee6a84d8a9f1ac848e4959899c9369c6aa
Benimana Jean Damascene <benidama02@gmail.com>
2025-11-20 21:21:42 +0300

    content added

--- src/components/Just.tsx
+++ src/components/Just.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Just() {
   return (
-    <div>Just</div>
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque accusantium consectetur reprehenderit. Sunt eum reiciendis vel maiores repudiandae in hic, placeat ipsam iste earum obcaecati repellat corrupti suscipit veritatis eveniet?</h1>
+    </div>
   )
 }
 
