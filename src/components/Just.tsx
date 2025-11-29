ced6d4fc56fccfe407c8c999b251ca674c2bfb63
Benimana Jean Damascene <benidama02@gmail.com>
2025-11-29 20:17:25 +0300

    all changes added well

--- src/components/Just.tsx
+++ src/components/Just.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
 
-function Just() {
+
+const Just = () => {
   return (
     <div>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque accusantium consectetur reprehenderit. Sunt eum reiciendis vel maiores repudiandae in hic, placeat ipsam iste earum obcaecati repellat corrupti suscipit veritatis eveniet?</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Neque accusantium consectetur reprehenderit. 
+        Sunt eum reiciendis vel maiores repudiandae in hic, placeat ipsam iste earum obcaecati repellat corrupti suscipit veritatis eveniet?
+        </h1>
+
+      <p>Try to learn Redux Toolkit step by step. And practice it regularly because practice is the key to 
+        success in learning any new technology.
+      </p>
     </div>
   )
 }
